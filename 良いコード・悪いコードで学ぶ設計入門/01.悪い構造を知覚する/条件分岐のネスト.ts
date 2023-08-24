5c3b048f57ce5865c4d557f3b94d93a51c6d7d6a
kosuke-17 <tamutamu17@icloud.com>
2023-08-24 09:43:19 +0900

    条件分岐のネスト

--- 良いコード・悪いコードで学ぶ設計入門/01.悪い構造を知覚する/条件分岐のネスト.ts
+++ 良いコード・悪いコードで学ぶ設計入門/01.悪い構造を知覚する/条件分岐のネスト.ts
@@ -1,11 +1,30 @@
 /**
- * 何重にもネストしたロジック
+ * 3重にネストしたロジック
+ *
+ * 複数行以上のネストはコードの見通しが悪くなる
+ *
+ * 生存確認しているロジックの中で魔法を発動するメソッドが実行されるのはよくない
+ *
  */
-const checkIfAlive = (member: { hitPoint: number; canAct: () => boolean }) => {
+const checkIfAlive = (member: Member, magic: Magic) => {
   if (member.hitPoint > 0) {
     // 行動可能かどうか
-    // 麻痺していたりするかの状態確認
+    // 眠り、麻痺の状態確認
     if (member.canAct()) {
+      // 魔法力が残存しているか判定
+      if (magic.constMagicPoint <= member.magicPoint) {
+        member.consumeMagicPoint(magic.costMagicPoint)
+        member.chant(magic)
+      }
     }
   }
 }
+
+type Member = {
+  hitPoint: number
+  magicPoint: number
+  canAct: () => boolean
+  consumeMagicPoint: (point: number) => void
+  chant: (magic: Magic) => void
+}
+type Magic = { costMagicPoint: number; constMagicPoint: number }
